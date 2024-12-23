d6388a05e7e6a9b52de978812216c7af0eebd8eb
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-12-23 20:59:45 +0530

    setup Electron and essential scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist-react'
+  },
+  server: {
+    port: 5123,
+    strictPort: true,
+  },
 })
